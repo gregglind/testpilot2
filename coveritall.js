f8c5110934c2cd261f94aaa5a288ece99f8a2160
Gregg Lind <gregg.lind@gmail.com>
2013-03-18 13:33:29 -0500

    coveritall no longer necessary.

--- coveritall.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// var HTMLReporter = require('../lib/reporters/HTMLReporter');
-
-coverobj = require("/Users/glind/tp2_coverstats.json");
-var HTMLReporter = require('./Coverjs/lib/reporters/HTMLReporter');
-//var reporter = new HTMLReporter(global.__$coverObject);
-var reporter = new HTMLReporter(coverobj);
-console.log(reporter.report());
